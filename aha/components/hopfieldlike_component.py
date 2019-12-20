Fix inh formula, clip after subtraction

aha/components/hopfieldlike_component.py
@@ -788,25 +788,20 @@ class HopfieldlikeComponent(Component):
     x_nn_shape = x_nn.get_shape().as_list()
     x_nn_size = np.prod(x_nn_shape[1:])
 
-    noise = tf.random_normal(shape=x_nn_shape, mean=0.0, stddev=0.1)
-    noise = tf.clip_by_value(noise, tf.reduce_min(x_nn), tf.reduce_max(x_nn))
-    # noise = print_minmax(noise, 'pr_noise')
+    noise = tf.random_normal(shape=x_nn_shape)
 
     random_recall = self._dual.add('random_recall', shape=[], default_value=False).add_pl(default=True, dtype=tf.bool)
-    # random_cue = self._dual.add('random_cue', shape=x_nn.shape, default_value=0.0).add_pl(default=True, dtype=x_nn.dtype)
     inhibition = self._dual.add('inhibition', shape=x_nn.shape, default_value=0.0).add_pl(default=True, dtype=x_nn.dtype)
     use_inhibition = self._dual.add('use_inhibition', shape=[], default_value=False).add_pl(default=True, dtype=tf.bool)
-    # inhibition = print_minmax(inhibition, 'inhibition')
 
     if self.use_inhibition:
-      decay = 0.99
-      inhibiton_decay_op = decay * inhibition + (1 - inhibition) * x_nn
+      decay = 0.9
+      inhibiton_decay_op = decay * inhibition + (1 - decay) * x_nn
       inhibition_decayed = tf.cond(tf.equal(use_inhibition, True), lambda: inhibiton_decay_op, lambda: tf.zeros_like(inhibition))
       self._dual.set_op('inhibition', inhibition_decayed)
 
     random_cue = noise - inhibition_decayed
-    # random_cue = print_minmax(random_cue, 'random_cue')
-    # x_nn = print_minmax(x_nn, 'x_nn')
+    random_cue = tf.clip_by_value(random_cue, 0.0, tf.reduce_max(random_cue))
 
     # Swap 'x_nn' during random recall
     x_nn = tf.cond(tf.equal(random_recall, True), lambda: random_cue, lambda: x_nn)