c31aaa63a85d0d16ead5003e1ba8ed53e1248819
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2020-01-14 18:26:00 +1100

    Updated def; add diameter/radius in csv output, improvements to parser (added args)

--- parse_results.py
+++ parse_results.py
@@ -1,7 +1,18 @@
 """Parse AHA results from Jenkins output."""
 
-input_path = 'jenkins.log'
-output_path = 'summary.csv'
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--input_path', type=str, help='an integer for the accumulator')
+
+args = parser.parse_args()
+
+if args.input_path is None or args.input_path == '' or not os.path.exists(args.input_path):
+  raise ValueError('Input path does not exist.')
+
+input_path = args.input_path
+output_path = input_path[:-4] + '.csv'
 
 
 with open(input_path, 'r') as f:
@@ -22,15 +33,18 @@ for num, line in enumerate(data):
     values.append(num)
     c += 1
 
-for i, j in zip(headings, values):
-  print(data[i])
-  print(data[j])
-  print('\n')
+# for i, j in zip(headings, values):
+#   print(data[i])
+#   print(data[j])
+#   print('\n')
+
+radius_increment = 0.05
 
 with open(output_path, 'w') as f:
-  f.write(data[headings[0]])
-  f.write('\n')
+  f.write('radius,diameter,' + data[headings[0]])
 
+  radius = 0
   for value in values:
-    f.write(data[value])
-    f.write('\n')
+    diameter = radius * 2
+    f.write(str(radius) + ',' + str(diameter) + ',' + data[value])
+    radius += radius_increment
