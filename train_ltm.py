47c27735db781448c13abb0bf6215e6d1f7e55e6
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2020-12-17 14:40:04 +1100

    add seed outer loop

--- train_ltm.py
+++ train_ltm.py
@@ -10,31 +10,36 @@ DEFINITION_PATH = 'definitions/epw-omni-pretrain-ll.json'
 
 
 def main():
-  experiment_prefix = datetime.datetime.now().strftime('%y%m%d-%H%M')
+  seeds = list(range(42, 50))
 
-  for r in range(1, NUM_RUNS + 1):
-    run_folder = 'run' + str(r).zfill(2)
+  for seed in seeds:
+    experiment_prefix = datetime.datetime.now().strftime('%y%m%d-%H%M')
+    print('Seed =', seed)
+    print('Prefix =', experiment_prefix)
 
-    experiment_opts_sweep = {
-        'seed': SEED
-    }
+    for r in range(1, NUM_RUNS + 1):
+      run_folder = 'run' + str(r).zfill(2)
 
-    workflow_opts_sweep = {
-        'evaluate_mode': ['simple', run_folder]
-    }
+      experiment_opts_sweep = {
+          'seed': seed
+      }
 
-    # now = datetime.datetime.now()
-    # summary_dir = 'summaries_' + now.strftime("%Y%m%d-%H%M%S") + '/'
-    summary_path = os.path.join('run', experiment_prefix, run_folder)
+      workflow_opts_sweep = {
+          'evaluate_mode': ['simple', run_folder]
+      }
 
-    subprocess.call([
-        'pagi',
-        'run',
-        '--experiment_def=' + DEFINITION_PATH,
-        '--workflow_opts_sweep=' + str(workflow_opts_sweep),
-        '--experiment_opts_sweep=' + str(experiment_opts_sweep),
-        '--summary_dir=' + summary_path
-    ])
+      # now = datetime.datetime.now()
+      # summary_dir = 'summaries_' + now.strftime("%Y%m%d-%H%M%S") + '/'
+      summary_path = os.path.join('run', experiment_prefix, run_folder)
+
+      subprocess.call([
+          'pagi',
+          'run',
+          '--experiment_def=' + DEFINITION_PATH,
+          '--workflow_opts_sweep=' + str(workflow_opts_sweep),
+          '--experiment_opts_sweep=' + str(experiment_opts_sweep),
+          '--summary_dir=' + summary_path
+      ])
 
 if __name__ == '__main__':
   main()
